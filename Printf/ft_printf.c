15e17887a95e664c3c162501046a9c24f4dc816a
Ethan7425 <ethanbernard25@gmail.com>
2022-11-09 14:21:49 +0100

    aide matteo

--- Printf/ft_printf.c
+++ Printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: etbernar <etbernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 09:43:19 by etbernar          #+#    #+#             */
-/*   Updated: 2022/11/09 13:08:41 by etbernar         ###   ########.fr       */
+/*   Updated: 2022/11/09 14:20:27 by etbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,16 +51,15 @@ int ft_printf(const char *suffix, ...)
 	
 	
 
-int main()
+/* int main()
 {
-   /* printf("1: hello my name is %s and i am %d old\n", "ethan", 20);
+	printf("1: hello my name is %s and i am %d old\n", "ethan", 20);
     printf("2: hello my name is %s and i am %d old\n", "ethan", 20);
     printf("1.1: %d\n", printf("something random.\n"));
     printf("2.1: %d\n", ft_printf("something random.\n"));
-     */
 
 	printf("COUNT EHTAN: %d\n", ft_printf("SALUT %s %c %u","salut",'e', 42)); 
 	printf("COUNT ORDI: %d\n", printf("SALUT %s %c %u","salut",'e', 42)); 
 
 
-}
+} */
