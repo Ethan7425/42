7c294eee505d2fb35048b5f46ffe16cd34e4a25c
Ethan7425 <ethanbernard25@gmail.com>
2022-12-16 15:14:08 +0100

    mouli gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,78 +6,64 @@
 /*   By: etbernar <etbernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 09:22:53 by etbernar          #+#    #+#             */
-/*   Updated: 2022/11/30 11:54:36 by etbernar         ###   ########.fr       */
+/*   Updated: 2022/12/16 15:13:35 by etbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
-
-
-
 #include "get_next_line.h"
 
-static char    *fill_line_buffer(int fd, char *left_c, char *buffer);
-static char    *set_line(char *line);
-static char    *ft_strchr(char *s, int c);
+static char	*ft_strchr(char *s, int c);
+static char	*fill_line_buffer(int fd, char *left_c, char *buffer);
+static char	*set_line(char *line);
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *left_c;
-    char        *line;
-    char        *buffer;
-    
-    buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-    /*
-     * fd < 0 : this means the file descriptor is invalid
-     * BUFFER_SIZE <= 0 : we'll read BUFFER_SIZE characters at a time, we can't read 0 or less character
-     * read(fd, 0, 0) < 0 : this check lets us see if the file exists and that it has some content to read from, or event that the file is openable to read, maybe the file descriptor is more than 0, but it was open in 'modify only', that means we can't read it.
-     */
-    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-    {
-        free(left_c);
-        free(buffer);
-        left_c = NULL;
-        buffer = NULL;
-        return (NULL);
-    }
-    if (!buffer)
-        return (NULL);
-    line = fill_line_buffer(fd, left_c, buffer);
-    // We have to free the buffer variable here since we'll not be using it later in the function, freeing it prevents memory leaks.
-    free(buffer);
-    buffer = NULL;
-    if (!line)
-        return (NULL);
-    left_c = set_line(line);
-    return (line);
+	static char	*left_c;
+	char		*line;
+	char		*buffer;
+
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(left_c);
+		free(buffer);
+		left_c = NULL;
+		buffer = NULL;
+		return (NULL);
+	}
+	if (!buffer)
+		return (NULL);
+	line = fill_line_buffer(fd, left_c, buffer);
+	free(buffer);
+	buffer = NULL;
+	if (!line)
+		return (NULL);
+	left_c = set_line(line);
+	return (line);
 }
 
-static char *set_line(char *line_buffer)
+static char	*set_line(char *line_buffer)
 {
-    char    *left_c;
-    ssize_t    i;
-    
-    i = 0;
-    while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
-        i++;
-    if (line_buffer[i] == 0 || line_buffer[1] == 0)
-        return (NULL);
-    left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
-    if (*left_c == 0)
-    {
-        free(left_c);
-        left_c = NULL;
-    }
-    line_buffer[i + 1] = 0;
-    return (left_c);
+	char	*left_c;
+	ssize_t	i;
+
+	i = 0;
+	while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
+		i++;
+	if (line_buffer[i] == 0 || line_buffer[1] == 0)
+		return (NULL);
+	left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
+	if (*left_c == 0)
+	{
+		free(left_c);
+		left_c = NULL;
+	}
+	line_buffer[i + 1] = 0;
+	return (left_c);
 }
 
 static char	*fill_line_buffer(int fd, char *left_c, char *buffer)
 {
-        /* ssize_t type works the same way as siyze_t type, but it can be a negative number, something that size_t can't do.
-        * Since most of the system function we'll be using return -1 to signify errors, it could be useful to be able to store negative numbers
-        */
 	ssize_t	read_buff;
 	char	*temp;
 
@@ -85,13 +71,11 @@ static char	*fill_line_buffer(int fd, char *left_c, char *buffer)
 	while (read_buff > 0)
 	{
 		read_buff = read(fd, buffer, BUFFER_SIZE);
-		// if read_buff is -1, it means there was an error reading the file descriptor, so we free left_c and return NULL.
 		if (read_buff == -1)
 		{
 			free(left_c);
 			return (NULL);
 		}
-		// if read_buff is 0, this surely means we read the whole file so there-s no need to stay in the loop
 		else if (read_buff == 0)
 			break ;
 		buffer[read_buff] = 0;
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,25 +6,24 @@
 /*   By: etbernar <etbernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 09:20:42 by etbernar          #+#    #+#             */
-/*   Updated: 2022/11/30 11:55:03 by etbernar         ###   ########.fr       */
+/*   Updated: 2022/12/16 15:13:34 by etbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H 
-#ifndef BUFFER_SIZE
+# ifndef BUFFER_SIZE
 # define BUFFER_SIZE=42 
 # endif
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 
-char    *get_next_line(int fd);
-size_t  ft_strlen(const char *str);
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 void	fill_str(char *res, char *s1, char *s2);
-
 
 #endif
