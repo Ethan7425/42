945d95848d082494d1ff2d33a59445723427a99f
Ethan7425 <ethanbernard25@gmail.com>
2023-01-03 11:13:05 +0100

    gnl bonus

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,27 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: etbernar <etbernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/23 09:20:42 by etbernar          #+#    #+#             */
-/*   Updated: 2023/01/03 10:37:56 by etbernar         ###   ########.fr       */
+/*   Created: 2023/01/03 10:30:37 by etbernar          #+#    #+#             */
+/*   Updated: 2023/01/03 10:39:33 by etbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
-# endif 
+# endif
+# define MAX_FD 10240
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 
 char	*get_next_line(int fd);
-size_t	ft_strlen(const char *str);
-char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 void	fill_str(char *res, char *s1, char *s2);
